119a9e2204eed99d273a0bbaebb7330edee713c6
NOMOREEXTOL <128805159+NOMOREEXTOL@users.noreply.github.com>
2023-06-13 21:11:01 +0800

    Add files via upload

--- Test3.py
+++ Test3.py
@@ -25,7 +25,7 @@ class Tools():
     """
     @staticmethod
     def sigmod(x): # 定义激活函数
-        return 1 / (1 + mt.exp(-x))
+        return 1 / (1 + np.exp(-x))
 
     @staticmethod
     def rand(low,high):
@@ -83,14 +83,28 @@ class Main:
 
         # 初始化原始数据数组
         with open(filePath,mode='r',encoding='utf-8') as f:
-            interArray = [i.strip().split('')[:-1] for i in f.readlines()]
-            self.output_label = np.array([i.strip().split('')[-1] for i in f.readlines()]).T
-        self.input_values = np.array(interArray).T
+            interArray1 = [i.strip().split(' ')[:-1] for i in f.readlines()]
+            f.seek(0)
+            interArray2 = [i.strip().split(' ')[-1:] for i in f.readlines()]
+        for i in range(len(interArray1)):
+            for j in range(len(interArray1[i])):
+                if not interArray1[i][j]:
+                    interArray1[i].remove(interArray1[i][j])
+                else:
+                    interArray1[i][j] = float(interArray1[i][j])
+        for i in range(len(interArray2)):
+            for j in range(len(interArray2[i])):
+                if not interArray2[i][j]:
+                    interArray2[i].remove(interArray2[i][j])
+                else:
+                    interArray2[i][j] = float(interArray2[i][j])
+        self.output_label = np.array(interArray2).T
+        self.input_values = np.array(interArray1).T
 
-        self.input_n = len(interArray[0])
+        self.input_n = len(interArray1[0])
         self.hidden_n = HN
         self.output_n = ON
-        self.X = len(interArray)
+        self.X = len(interArray1)
 
         # 初始化相关矩阵
         self.hidden_weights = np.array([[0.0 for __ in range(self.input_n)] for _ in range(self.hidden_n)])
@@ -114,7 +128,7 @@ class Main:
 
         for h in range(self.hidden_n):
             for o in range(self.output_n):
-                self.output_weighs[o][h] = Tools.rand(-1,1)
+                self.output_weights[o][h] = Tools.rand(-1,1)
 
 
     def forward(self): # 前向传播
@@ -123,6 +137,7 @@ class Main:
         # 隐含层激活
         for h in range(self.hidden_n):
             for j in range(self.X):
+                print(self.hidden_in_values[h][j])
                 self.hidden_values[h][j] = Tools.sigmod(self.hidden_in_values[h][j])
         # 隐含层到输出层
         self.output_in_values = self.output_weights @ self.hidden_values + self.output_bias
@@ -145,18 +160,19 @@ class Main:
         self.output_error = self.output_values - self.output_label # 即sigamaL 由sigamaL可迭代求出sigamaL-1即上一层的error
         # 计算输出层的权重，偏置修正量
         self.output_weights = self.output_weights - Main.LEARN_RATE * self.output_error @ (self.hidden_values.T)
-        self.output_bias = self.output_bias - Main.LEARN_RATE @ self.output_error
+        self.output_bias = self.output_bias - Main.LEARN_RATE * self.output_error
         # 计算隐含层的等效误差
         self.hidden_error = (self.output_weights.T) @ self.output_error * self.hidden_values
         # (HN,x) = (HN,ON) @ (ON,x) * (HN,x) 矩阵的点乘与叉乘
         # 计算隐含层的权重，偏置修正量
         self.hidden_weights = self.hidden_weights - Main.LEARN_RATE * self.hidden_error @ (self.input_values.T)
-        self.hidden_bias = self.hidden_bias - Main.LEARN_RATE @ self.hidden_error
+        self.hidden_bias = self.hidden_bias - Main.LEARN_RATE * self.hidden_error
 
         # 校验样本总体均方误差是否合格，合格就退出迭代，不合格则修正w,b继续迭代
 
+
     def train(self):
-        self.setup(r'C:\ Users\xiaomi\Desktop\test.txt',4,1) # 输入数据
+        self.setup(r'C:\Users\xiaomi\Desktop\test.txt',4,1) # 输入数据
         circleTimes = 0
         self.forward() # 前向传播
         self.backPropagate() # 反向传播
@@ -174,16 +190,19 @@ class Main:
 
 
 
-
-
-
-
-
-
-
-
-
 if __name__ == "__main__":
-    f = open(r'C:\Users\xiaomi\Desktop\test.txt', mode='r', encoding='utf-8')
-    interArray = [i.strip().split()[:-1] for i in f.readlines()]
-    print(np.array(interArray).T)
+    s = Main()
+    s.train()
+
+    """
+    possible output can be follows:
+    -1373.2949419512345
+    -3.3632961514100277
+    -77.4035165804202
+    -91.8688115216147
+    -106.3335065452646
+    -91.8688115216147
+    当前迭代次数为1949
+    误差为:[-0.00990536 -0.00230966  0.00111321  0.00966043]
+    当前输出值为:[[0.99009464 0.49769034 0.50111321 0.00966043]]
+    """
